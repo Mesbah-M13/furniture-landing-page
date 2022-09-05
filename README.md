17d53412beceb558a9d6306659a42792f475b3b7
Mesbah-Himel <mesbah0010@gmail.com>
2022-09-05 15:36:39 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Want to see in live action? <a target="_blank" href="https://sunny-jalebi-cf7c89.netlify.app/">Live</a>
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Features from './components/Features';
+import FeatureSecond from './components/FeatureSecond';
+import Hero from './components/Hero';
+import NewItems from './components/NewItems';
+import Products from './components/Products';
+import Testimonial from './components/Testimonial';
+import Home from './pages/Home';
+import Newsletter from './components/Newsletter';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <>
+      <Home />
+      <Hero />
+      <Features />
+      <NewItems />
+      <FeatureSecond />
+      <Products />
+      <Testimonial />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
